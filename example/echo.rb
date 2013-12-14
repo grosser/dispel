17cc873f3afb909213581b7533e1d25c407d744a
grosser <michael@grosser.it>
2013-12-13 19:23:07 -0800

    example for readme

--- example/echo.rb
+++ example/echo.rb
@@ -1,16 +1,3 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require 'dispel'
-
-# draw app and redraw after each keystroke
-Dispel::Screen.open do |screen|
-  screen.draw "Initial state, press any key!\n\n   Oh boy!"
-
-  Dispel::Keyboard.output do |key|
-    case key
-    when :resize then screen.clear_cache
-    when :"Ctrl+c" then break
-    end
-
-    screen.draw "Looks like you pressed #{key} (Ctrl+c to quit)"
-  end
-end
+code = File.read("Readme.md")[/<!-- example -->\n```Ruby(.*)```\n<!-- example -->/m, 1]
+eval code
